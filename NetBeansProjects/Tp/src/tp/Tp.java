49c91c04cfbcdd11adfd63170c19b2854721e018
DeathScytheZ <digital1808ta@gmail.com>
2024-10-15 11:01:49 +0100

    first commit

---
+++ NetBeansProjects/Tp/src/tp/Tp.java
@@ -0,0 +1,11 @@
+package tp;
+import javax.swing.JFrame;
+        
+public class Tp {
+    public static void main(String[] args) {
+        Etudient Et = new Etudient();
+        Et.setVisible(true);
+        
+    }
+    
+}
