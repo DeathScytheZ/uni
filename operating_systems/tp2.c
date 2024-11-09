bcb3f66fec3faf4917f749e2765de1ed7f4587bb
DeathScytheZ <digital1808ta@gmail.com>
2024-11-09 10:19:09 +0100

    .

--- operating_systems/tp2.c
+++ operating_systems/tp2.c
@@ -15,10 +15,9 @@ void* T3(){
 void main(){
 	pthread_t thread1, thread2, thread3;
 	pthread_create(&thread1, NULL, *T1, NULL);
-	pthread_create(&thread2, NULL, *T2, NULL);
-	pthread_create(&thread3, NULL, *T3, NULL);
 	pthread_join(thread1, NULL);
+	pthread_create(&thread2, NULL, *T2, NULL);
 	pthread_join(thread2, NULL);
+	pthread_create(&thread3, NULL, *T3, NULL);
 	pthread_join(thread3, NULL);
-	printf("main_thread");
 }
