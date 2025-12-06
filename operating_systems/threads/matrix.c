3e21bbe9ef488071fc2c54179bacb09d71da13e6
DeathScytheZ <digital1808ta@gmail.com>
2025-12-06 13:50:42 +0100

    .

--- operating_systems/threads/matrix.c
+++ operating_systems/threads/matrix.c
@@ -1,44 +1,34 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define N 10
 #define M 10
 
-typedef struct {
-	int result[M];
-	int n;
-} ROW;
-
 int matrix1[N][M], matrix2[N][M], matrix3[N][M]; 
 
 void* routine(void* arg){
-    ROW row = *(ROW*) arg;
+    int n = *(int*) arg;
     for(int i = 0; i < M; i++){
-        row.result[i] = matrix1[row.n][i] + matrix2[row.n][i]; 
+        matrix3[n][i] = matrix1[n][i] + matrix2[n][i]; 
     }
     pthread_exit(NULL);
 } 
 
 int main(){
+    pthread_t threads[N];
+    int ids[N];
     srand(time(NULL));
     for(int i = 0; i < N; i++){
+	ids[i] = i;
 	for(int j = 0; j < M; j++){
 	    matrix1[i][j] = rand() % 100 + 1;
 	    matrix2[i][j] = rand() % 100 + 1;
 	}
     }
-    pthread_t threads[N];
-    ROW rows[N];
-	
+
     for(int i = 0; i < N; i++){
-	rows[i].n = i;
-        pthread_create(&threads[i], NULL, routine, (void*) &rows[i]);
+        pthread_create(&threads[i], NULL, routine, (void*) &ids[i]);
     }
-    for(int i = 0; i < N; i++){
-        pthread_join(threads[i], NULL); 
-	for(int j = 0; j < M; j++){
-		matrix3[i][j] = rows[i].result[j];
-	}
-}
 }
